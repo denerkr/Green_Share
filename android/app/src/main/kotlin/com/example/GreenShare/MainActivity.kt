cd672bb77edbdb15c0690f3d4adbfd51c6ae8056
denerkr <denerkr@gmail.com>
2020-10-08 09:59:55 -0300

    start

---
+++ android/app/src/main/kotlin/com/example/GreenShare/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.GreenShare
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MaterialApp(
+      home: Scaffold(
+        body: ListadeTransferencias(),
+        appBar: AppBar(
+          title: Text('Transferências'),
+        ),
+        floatingActionButton: FloatingActionButton(
+          child: Icon(Icons.add),
+        ),
+      ),
+    ));
+
+class ListadeTransferencias extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        ItemTransferencia(Transferencia(100.00, 100.000)),
+        ItemTransferencia(Transferencia(200.00, 200.000)),
+        ItemTransferencia(Transferencia(300.00, 500.000)),
+      ],
+    );
+  }
+}
+
+class ItemTransferencia extends StatelessWidget {
+  final Transferencia _transferencia;
+
+  ItemTransferencia(this._transferencia);
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: ListTile(
+        leading: Icon(Icons.monetization_on),
+        title: Text(_transferencia.valor.toString()),
+        subtitle: Text(_transferencia.numeroconta.toString()),
+      ),
+    );
+  }
+}
+
+class Transferencia {
+  final double valor;
+  final double numeroconta;
+
+  Transferencia(this.valor, this.numeroconta);
+}
