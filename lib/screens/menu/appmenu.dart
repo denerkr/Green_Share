cbf6422dabb8e8105e138940670a9f649f9ba5a0
denerkr <denerkr@gmail.com>
2020-11-25 14:04:32 -0300

    up 25-11

--- lib/screens/menu/appmenu.dart
+++ lib/screens/menu/appmenu.dart
@@ -2,12 +2,62 @@ import 'package:GreenShare/models/transferencia.dart';
 import 'package:GreenShare/screens/transferencia/formulario.dart';
 import 'package:GreenShare/screens/transferencia/lista.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_database/firebase_database.dart';
 
 class Menu extends StatefulWidget {
   @override
   State<StatefulWidget> createState() {
     return MenuState();
   }
+}
+
+  final databaseReference = FirebaseDatabase.instance.reference();
+
+void createData(){
+  databaseReference.child("flutterDevsTeam1").set({
+    'name': 'Deepak Nishad',
+    'description': 'Team Lead'
+  });
+  databaseReference.child("flutterDevsTeam2").set({
+    'name': 'Yashwant Kumar',
+    'description': 'Senior Software Engineer'
+  });
+  databaseReference.child("flutterDevsTeam3").set({
+    'name': 'Akshay',
+    'description': 'Software Engineer'
+  });
+  databaseReference.child("flutterDevsTeam4").set({
+    'name': 'Aditya',
+    'description': 'Software Engineer'
+  });
+  databaseReference.child("flutterDevsTeam5").set({
+    'name': 'Shaiq',
+    'description': 'Associate Software Engineer'
+  });
+  databaseReference.child("flutterDevsTeam6").set({
+    'name': 'Mohit',
+    'description': 'Associate Software Engineer'
+  });
+  databaseReference.child("flutterDevsTeam7").set({
+    'name': 'Naveen',
+    'description': 'Associate Software Engineer'
+  });
+}
+
+void Apagar() async {
+  databaseReference.child('flutterDevsTeam1').remove();
+  databaseReference.child('flutterDevsTeam2').remove();
+  databaseReference.child('flutterDevsTeam3').remove();
+  databaseReference.child('flutterDevsTeam4').remove();
+  databaseReference.child('flutterDevsTeam5').remove();
+  databaseReference.child('flutterDevsTeam6').remove();
+  databaseReference.child('flutterDevsTeam7').remove();
+}
+
+void readData(){
+  databaseReference.child("usuarios").child("residencia6").child("cadastro").child("-MLuCXL5qbtHL-Ngbm1Z").once().then((DataSnapshot usuarios) {
+    print('Data : ${usuarios.value}');
+  });
 }
 
 class MenuState extends State<Menu> {
@@ -40,6 +90,14 @@ class MenuState extends State<Menu> {
                       return FormularioTransferencia();
                     }));
                   }),
+              RaisedButton(
+                  child: Text('Criar Devs no firebase'),
+                  onPressed: () => createData(),
+              ),
+              RaisedButton(
+                child: Text('Apagar Devs no firebase'),
+                onPressed: () => Apagar(),
+              ),
             ],
           ),
         ));
--- lib/screens/transferencia/formulario.dart
+++ lib/screens/transferencia/formulario.dart
@@ -60,43 +60,6 @@ class FormularioTransferenciaState extends State<FormularioTransferencia> {
   }
 }
 
-void createData(){
-  databaseReference.child("flutterDevsTeam1").set({
-    'name': 'Deepak Nishad',
-    'description': 'Team Lead'
-  });
-  databaseReference.child("flutterDevsTeam2").set({
-    'name': 'Yashwant Kumar',
-    'description': 'Senior Software Engineer'
-  });
-  databaseReference.child("flutterDevsTeam3").set({
-    'name': 'Akshay',
-    'description': 'Software Engineer'
-  });
-  databaseReference.child("flutterDevsTeam4").set({
-    'name': 'Aditya',
-    'description': 'Software Engineer'
-  });
-  databaseReference.child("flutterDevsTeam5").set({
-    'name': 'Shaiq',
-    'description': 'Associate Software Engineer'
-  });
-  databaseReference.child("flutterDevsTeam6").set({
-    'name': 'Mohit',
-    'description': 'Associate Software Engineer'
-  });
-  databaseReference.child("flutterDevsTeam7").set({
-    'name': 'Naveen',
-    'description': 'Associate Software Engineer'
-  });
-}
-
-void readData(){
-  databaseReference.child("usuarios").child("residencia6").child("cadastro").child("-MLuCXL5qbtHL-Ngbm1Z").once().then((DataSnapshot usuarios) {
-      print('Data : ${usuarios.value}');
-  });
-}
-
 Future<String> getsenha() async {
   String result = (await FirebaseDatabase.instance.reference().child("usuarios/residencia6/cadastro/-MLuCXL5qbtHL-Ngbm1Z/senha").once()).value;
   print(result);
